16956aad22aee79ffde5a05f65cd5afff22d5fff
Ryan Couto <r4couto@gmail.com>
2015-07-01 16:50:01 -0400

    Create snake-view constructor.

---
+++ js/snake-view.js
@@ -0,0 +1,13 @@
+(function () {
+
+  if (typeof SnakeGame === "undefined"){
+    window.SnakeGame = {};
+  }
+
+  var View = SnakeGame.View = function ($el, board) {
+    this.$el = $el;
+    this.board = board;
+  }
+
+
+})();
