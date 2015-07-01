92a420dec275d672018375bbdce38b465b52675c
Ryan Couto <r4couto@gmail.com>
2015-07-01 16:29:05 -0400

    Add Board#appleGen.

--- js/board.js
+++ js/board.js
@@ -11,9 +11,23 @@
       Math.floor(Math.random() * dimX), Math.floor(Math.random() * dimY)
     ];
     this.snake = new SnakeGame.Snake(randomPos);
+    this.applePos = this.appleGen();
   }
 
-  
+  Board.prototype.appleGen = function () {
+    var randomPos = [
+      Math.floor(Math.random() * dimX), Math.floor(Math.random() * dimY)
+    ];
+    if (this.snake.body.some( function (bodyEl) {
+      return (SnakeGame.Coord.equals(randomPos, bodyEl))
+    })) {
+      this.appleGen();
+    } else {
+      this.applePos = randomPos;
+    }
+  }
+
+
 
 
 })();
