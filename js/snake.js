9058a6a5c276f7c338978878b919cc8a19a53c6f
Ryan Couto <r4couto@gmail.com>
2015-07-01 16:10:55 -0400

    Add Snake#move, #turn, #collided.

--- js/snake.js
+++ js/snake.js
@@ -8,5 +8,22 @@
     this.dir = undefined;
   }
 
+  Snake.prototype.move = function () {
+    var nextHeadPos = SnakeGame.Coord.add(this.body[0]. this.dir);
+    this.body.pop();
+    this.body.unshift(nextHeadPos);
+  }
 
+  Snake.prototype.turn = function(newDir) {
+    if (SnakeGame.Coord.opposite(this.dir, newDir)) { return; }
+    this.dir = newDir;
+  }
+
+  Snake.prototype.collided = function() {
+    return this.body.slice(1).some( function(el) {
+      return SnakeGame.Coord.equals(el, this.body[0]);
+    }.bind(this));
+  }
+
+  
 })();
