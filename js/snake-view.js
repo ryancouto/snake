0c6493e02a9226643b2c3ada58bcbf74b8535b42
Ryan Couto <r4couto@gmail.com>
2015-07-01 17:01:53 -0400

    Add View#setupBoard.

--- js/snake-view.js
+++ js/snake-view.js
@@ -7,6 +7,24 @@
   var View = SnakeGame.View = function ($el, board) {
     this.$el = $el;
     this.board = board;
+    this.setupBoard();
+  }
+
+  View.prototype.setupBoard = function() {
+    var $boardEl = this.$el.find('.board');
+    for (i = 0; i < this.board.dimX; i++){
+      for (var j = 0; j < this.board.dimY; j++)
+        var el = $("<li></li>");
+        if (SnakeGame.Coord.equals([i,j], this.board.snake.head())){
+          $boardEl.addClass("snake");
+        }
+        $boardEl.append(el);
+      }
+    }
+  }
+
+  View.prototype.installListeners = function() {
+
   }
 
 
